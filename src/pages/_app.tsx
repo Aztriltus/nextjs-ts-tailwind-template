73aec916c963e289864a54f4015a329b3bbe77ac
sushanyadav <sushanyadav98@gmail.com>
2022-08-02 19:01:14 +0545

    ♿️ accessibility: fixed Viewport meta tag lighthouse score issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,10 +10,7 @@ export const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <meta
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-          name="viewport"
-        />
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <PageHead />
       </Head>
       <Layout>
