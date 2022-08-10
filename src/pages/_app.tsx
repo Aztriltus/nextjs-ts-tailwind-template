f71727a125218c70e636c011ad337f6e5fbcf54b
Andre H <andrehadiantolesmana@gmail.com>
2022-08-10 14:33:53 +0800

    chore: integrate theme to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,30 @@
 import "@/styles/globals.scss";
 
+import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
 
 import { CoreLayout } from "@/common/components/CoreLayout";
 import { PageHead } from "@/common/components/PageHead";
+import { ChakraFonts } from "@/common/components/CustomFont";
+
+import theme from "@/theme";
 
 export const App = ({ Component, pageProps }) => {
   const Layout = Component.layout ? Component.layout : CoreLayout;
   return (
-    <>
+    <ChakraProvider theme={theme}>
+      <ChakraFonts />
       <Head>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <meta
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          name="viewport"
+        />
         <PageHead />
       </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </ChakraProvider>
   );
 };
 
