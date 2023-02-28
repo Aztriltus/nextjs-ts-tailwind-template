23d0869b75e6fa7ca669d1147ca37b9bb2d59a76
Zachery Ng <zachery.ng@gmail.com>
2023-02-28 17:26:53 +0800

    chore: update eslint, prettier and dev dependencies

---  .prettierrc.js
+++  .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
+  singleQuote: true,
   trailingComma: "all",
   arrowParens: "always",
-  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 80,
   jsxSingleQuote: false,
+  plugins: [require("prettier-plugin-tailwindcss")],
 };
